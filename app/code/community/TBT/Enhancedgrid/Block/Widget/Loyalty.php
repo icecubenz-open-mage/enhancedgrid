121a157d91d290ffa8e247d442d38c2d89fd4f3e
Robert Coleman <git@robert.net.nz>
2021-01-10 16:05:03 +1300

    stub out loyalty

--- app/code/community/TBT/Enhancedgrid/Block/Widget/Loyalty.php
+++ app/code/community/TBT/Enhancedgrid/Block/Widget/Loyalty.php
@@ -12,54 +12,16 @@ class TBT_Enhancedgrid_Block_Widget_Loyalty extends Mage_Adminhtml_Block_Templat
 {
     public function _toHtml()
     {
-        $html = <<<FEED
-	    	<!-- Visit https://www.wdca.ca/m for information about this frame.  You can remove it if you want. -->
-        	<iframe src="{$this->_getLoyaltyUrl()}" marginwidth="0" marginheight="0"
-                	align="middle" frameborder="0"
-                    scrolling="no" style="width: 500px; float: left; height: 22px;">
-            </iframe>
-FEED;
-
-        return $html;
+        return '';
     }
 
     protected function _getLoyaltyUrl()
     {
-        $url = $this->_getBaseLoyaltyUrl();
-
-        $url_data = array();
-        $url_data['a'] = 'enhancedgrid';
-        $url_data['v'] = (string) Mage::getConfig()->getNode('modules/TBT_Enhancedgrid/version');
-        $url_data['m'] =  Mage::getVersion();
-        $url_data['p'] =  urlencode($this->getBaseUrl());
-        $url_data['ap'] =  urlencode($this->getAction()->getFullActionName());
-        //$url_data["license"] =  Mage::helper('rewards/loyalty_checker')->getLicenseKey();
-
-        $url_data_json = json_encode($url_data);
-
-        $salt = 'welovewdca12345!!';
-
-        $url_data_json_hex = bin2hex($url_data_json.$salt);
-
-        $url = $url.'?data='.$url_data_json_hex;
-
-        return $url;
+        return '';
     }
 
     protected function _getBaseLoyaltyUrl()
     {
-        $url = 'https://www.wdca.ca/m';
-
-        //@nelkaake: If the page is supposed to be HTTPS and the AJAX call is not HTTPS, add HTTPS
-        // if it's HTTP and the url returned HTTPS, remove HTTPS
-        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && strpos(strtolower($url), 'https') !== 0) {
-            $url = str_replace('http', 'https', $url);
-        } elseif (!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS'] && strpos(strtolower($url), 'https') === 0) {
-            $url = str_replace('https', 'http', $url);
-        } else {
-            // the url is fine and we can continue because it's using the correct encryption
-        }
-
-        return $url;
+        return '';
     }
 }
